84f40aa14021d9af6c8436007355e4730e47049a
phanthiquetran <quetranphan030104@gmail.com>
2025-08-17 21:22:16 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
